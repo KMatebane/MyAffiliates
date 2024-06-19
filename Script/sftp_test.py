bca4546093f9d2ad7806f8011d0ac770c72ce746
KMatebane <katlego.matebane@betika.com>
2024-06-19 17:30:40 +0200

    upload test file

---
+++ Script/sftp_test.py
@@ -0,0 +1,13 @@
+def sftp_connection():
+    cnopts = pysftp.CnOpts()
+    server = pysftp.Connection('ftp.myaffiliates.com', username='betika_data', password='P1QY74p7XwUezEcO', cnopts=cnopts)  ############# what's the password
+    return server
+
+def test_sftp_connection():
+    try:
+        server = sftp_connection()
+        server.cwd('/myaffiliates/betika/data/queue')  ############## change the directory
+        print("SFTP connection test successful")
+        server.close()
+    except Exception as e:
+        print("SFTP connection test failed:", str(e))
