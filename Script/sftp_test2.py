5d0b41cb59c1aecf0b363fce70c9692aa4cfe80b
KMatebane <katlego.matebane@betika.com>
2024-06-20 12:16:41 +0200

    Update sftp_test2.py

--- Script/sftp_test2.py
+++ Script/sftp_test2.py
@@ -1,10 +1,13 @@
 import pysftp
+import paramiko
 
+#Optimove
 def sftp_connection():
     cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDER1AM/lVwpZIDmjtIXWMMY8mN++RKjBEZNR+jroGwZP6GMIOf1mRgvJ8sszMsLCgPQ7jNzqcPfhaoifBe/Tw3m5zg5UueCluQR3Ttj3n8BIyojvRtyYncE80TOEzku+6TGZFQAl8m53psMJhnlFDN7dtegoFn2LTPfFCEC9xBlnlR6l3OPdE85ni4PfTbt5aeivX2dz32BVGP7QWSujDYDmB4fIzE86mWz8nv6ZZKxYKqjNQarOyBK9DPNLl8MvhsumkF7Mcps62jJXhLPVZ1/5uwHSxRcIVSankbelrrz4iKrf7Ff0HJdnvmxlM2nVHRtgU/tr3xX1Ut6Q/OkQ43'
-    server = pysftp.Connection('ftp.myaffiliates.com', username='betika_data', password='P1QY74p7XwUezEcO', cnopts=cnopts)  ############# what's the password
+    cnopts.hostkeys = None
+    server = pysftp.Connection('ftp.myaffiliates.com', username='betika_data', password='P1QY74p7XwUezEcO', cnopts=cnopts)
     return server
+
 
 def test_sftp_connection():
     try:
@@ -14,8 +17,3 @@ def test_sftp_connection():
         server.close()
     except Exception as e:
         print("SFTP connection test failed:", str(e))
-
-test_sftp_connection()
-
-sftpHost = ''
-sftpPort = ''
