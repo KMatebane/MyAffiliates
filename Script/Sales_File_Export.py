5b78eca7a2a5337575fc7200f2d7c1d287384cd0
KMatebane <katlego.matebane@betika.com>
2024-07-03 13:41:00 +0200

    update scripts

--- Script/Sales_File_Export.py
+++ Script/Sales_File_Export.py
@@ -40,29 +40,49 @@ try:
                                 ,x.PLAYER_CURRENCY\
                                 ,x.ROLLBACKS\
                                 ,x.FIRST_DEPOSIT_AMOUNT\
-                                ,x.TOTAL_DEPOSITS\
+                                ,x.DEPOSITS\
                                 ,x.SPORTSBOOK_BETS	\
                                 ,x.SPORTSBOOK_STAKE	\
                                 ,x.SPORTSBOOK_GROSS_REVENUE	\
-                                ,x.SPORTSBOOK_BONUS	\
-                                ,(x.SPORTSBOOK_GROSS_REVENUE - x.SPORTSBOOK_BONUS) AS SPORTSBOOK_NET_REVENUE	\
+                                ,COALESCE(y.SPORTSBOOK_BONUS,0)	                                AS SPORTSBOOK_BONUS\
+                                ,(x.SPORTSBOOK_GROSS_REVENUE - COALESCE(y.SPORTSBOOK_BONUS,0))  AS SPORTSBOOK_NET_REVENUE	\
                                 ,x.CRASH_BETS	\
                                 ,x.CRASH_STAKE	\
                                 ,x.CRASH_GROSS_REVENUE	\
-                                ,x.CRASH_BONUS	\
-                                ,(x.CRASH_GROSS_REVENUE - x.CRASH_BONUS) AS CRASH_NET_REVENUE	\
+                                ,COALESCE(y.CRASH_BONUS,0) AS CRASH_BONUS\
+                                ,(x.CRASH_GROSS_REVENUE - COALESCE(y.CRASH_BONUS,0))            AS CRASH_NET_REVENUE	\
                                 ,x.CASINO_BETS	\
                                 ,x.CASINO_STAKE	\
                                 ,x.CASINO_GROSS_REVENUE	\
-                                ,x.CASINO_BONUS	\
-                                ,(x.CASINO_GROSS_REVENUE - x.CASINO_BONUS) AS CASINO_NET_REVENUE	\
+                                ,COALESCE(y.CASINO_BONUS, 0)                                    AS CASINO_BONUS	\
+                                ,(x.CASINO_GROSS_REVENUE - COALESCE(y.CASINO_BONUS,0))          AS CASINO_NET_REVENUE	\
                                 ,x.VIRTUALS_BETS	\
                                 ,x.VIRTUALS_STAKES	\
                                 ,x.VIRTUALS_GROSS_REVENUE	\
-                                ,x.VIRTUALS_BONUS\
-                                ,(x.VIRTUALS_GROSS_REVENUE - x.VIRTUALS_BONUS) AS VIRTUALS_NET_REVENUE\
+                                ,COALESCE(y.VIRTUALS_BONUS,0) AS VIRTUALS_BONUS\
+                                ,(x.VIRTUALS_GROSS_REVENUE - COALESCE(y.VIRTUALS_BONUS,0))      AS VIRTUALS_NET_REVENUE\
 \
-                                FROM (SELECT a.summary_date                             AS TRANSACTION_DATE\
+                        FROM (SELECT  z.TRANSACTION_DATE\
+                                    ,z.BTAG\
+                                    ,z.PLAYER_ID\
+                                    ,z.PLAYER_CURRENCY\
+                                    ,z.ROLLBACKS\
+                                    ,z.FIRST_DEPOSIT_AMOUNT\
+                                    ,z.DEPOSITS\
+                                    ,z.SPORTSBOOK_BETS	\
+                                    ,z.SPORTSBOOK_STAKE	\
+                                    ,z.SPORTSBOOK_GROSS_REVENUE	\
+                                    ,z.CRASH_BETS	\
+                                    ,z.CRASH_STAKE	\
+                                    ,z.CRASH_GROSS_REVENUE	\
+                                    ,z.CASINO_BETS	\
+                                    ,z.CASINO_STAKE	\
+                                    ,z.CASINO_GROSS_REVENUE	\
+                                    ,z.VIRTUALS_BETS	\
+                                    ,z.VIRTUALS_STAKES	\
+                                    ,z.VIRTUALS_GROSS_REVENUE	\
+\
+                            FROM (SELECT a.summary_date                             AS TRANSACTION_DATE\
                                         ,c.affiliate_tag                                AS BTAG\
                                         ,a.profile_id                                   AS PLAYER_ID	\
                                         ,'GHS'                                          AS PLAYER_CURRENCY\
@@ -70,27 +90,25 @@ try:
                                         ,CASE WHEN e.first_deposit = a.summary_date \
                                             THEN a.dep ELSE 0 \
                                             END AS FIRST_DEPOSIT_AMOUNT\
-                                        ,a.dep                                          AS TOTAL_DEPOSITS\
+                                        ,a.dep                                          AS DEPOSITS\
                                         ,a.qty_sp_cash                                  AS SPORTSBOOK_BETS	\
                                         ,a.to_sp_cash                                   AS SPORTSBOOK_STAKE	\
-                                        ,(a.GGR_sp - (a.GGR_sp * 0.25))                 AS SPORTSBOOK_GROSS_REVENUE	\
-                                        ,d.sp_cash_bonus_amt                            AS SPORTSBOOK_BONUS	\
+                                        ,(a.GGR_sp - (a.GGR_sp * 0.25))                 AS SPORTSBOOK_GROSS_REVENUE		\
                                         ,a.NGR_sp                                       AS SPORTSBOOK_NET_REVENUE	\
                                         ,a.qty_cr                                       AS CRASH_BETS	\
                                         ,a.to_cr                                        AS CRASH_STAKE	\
-                                        ,(a.GGR_cr - (a.GGR_cr * 0.25))                 AS CRASH_GROSS_REVENUE	\
-                                        ,d.cr_cash_bonus_amt                            AS CRASH_BONUS	\
+                                        ,(a.GGR_cr - (a.GGR_cr * 0.25))                 AS CRASH_GROSS_REVENUE		\
                                         ,a.NGR_cr                                       AS CRASH_NET_REVENUE	\
                                         ,a.qty_ca                                       AS CASINO_BETS	\
                                         ,a.to_ca                                        AS CASINO_STAKE	\
-                                        ,(a.GGR_ca - (a.GGR_ca * 0.25))                 AS CASINO_GROSS_REVENUE	\
-                                        ,d.ca_cash_bonus_amt                            AS CASINO_BONUS	\
+                                        ,(a.GGR_ca - (a.GGR_ca * 0.25))                 AS CASINO_GROSS_REVENUE		\
                                         ,a.NGR_ca                                       AS CASINO_NET_REVENUE	\
                                         ,a.qty_v                                        AS VIRTUALS_BETS	\
                                         ,a.to_v                                         AS VIRTUALS_STAKES	\
                                         ,(a.GGR_v - (a.GGR_v * 0.25))                   AS VIRTUALS_GROSS_REVENUE	\
-                                        ,d.vi_cash_bonus_amt                            AS VIRTUALS_BONUS\
                                         ,a.NGR_v                                        AS VIRTUALS_NET_REVENUE\
+                                        ,c.created\
+                                        ,RANK() OVER (PARTITION BY c.profile_id ORDER BY c.created DESC) AS Ranking\
 \
                                 FROM betika_bi_gh.f_kpi_gh AS a\
 \
@@ -100,23 +118,49 @@ try:
                                 LEFT JOIN betika_bi_gh.affiliate_tx AS c\
                                 ON a.profile_id = c.profile_id\
 \
-                                LEFT JOIN betika_bi_gh.f_bonus_credited AS d\
-                                ON a.profile_id = d.profile_id\
-\
                                 LEFT JOIN betika_bi_gh.dim_first_last_gh AS e\
                                 ON a.profile_id = e.profile_id\
 \
                                 WHERE DATE(a.summary_date) >= DATE(CURDATE()- INTERVAL 1 DAY)\
-                                AND DATE(d.summary_date) >= DATE(CURDATE()- INTERVAL 1 DAY)\
-                         \
+\
                                 AND (c.affiliate_tag IS NOT NULL AND c.affiliate_tag NOT IN ('None', 'Null'))\
 \
-                                GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27\
+                                GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24\
 \
                                 ORDER BY a.profile_id\
                                         ,a.summary_date\
                                 \
-                                ) AS x;"
+                                ) AS z\
+                                WHERE z.Ranking = 1\
+                            ) AS x\
+\
+                        LEFT OUTER JOIN (SELECT z.PLAYER_ID\
+                                            ,z.SPORTSBOOK_BONUS\
+                                            ,z.CRASH_BONUS\
+                                            ,z.CASINO_BONUS\
+                                            ,z.VIRTUALS_BONUS\
+\
+                                        FROM (SELECT a.profile_id AS PLAYER_ID\
+                                                    ,a.sp_cash_bonus_amt                            AS SPORTSBOOK_BONUS\
+                                                    ,a.cr_cash_bonus_amt                            AS CRASH_BONUS\
+                                                    ,a.ca_cash_bonus_amt                            AS CASINO_BONUS\
+                                                    ,a.vi_cash_bonus_amt                            AS VIRTUALS_BONUS\
+                                                    ,b.created\
+                                                    ,RANK() OVER (PARTITION BY b.profile_id ORDER BY b.created DESC) AS Ranking \
+                                        \
+                                            FROM betika_bi_gh.f_bonus_credited AS a\
+                                            LEFT JOIN betika_bi_gh.affiliate_tx AS b\
+                                            ON a.profile_id = b.profile_id\
+                                            \
+                                            WHERE DATE(a.summary_date) >= DATE(CURDATE()- INTERVAL 1 DAY)\
+                                            AND (b.affiliate_tag IS NOT NULL AND b.affiliate_tag NOT IN ('None', 'Null'))\
+                                            GROUP BY 1,2,3,4,5,6\
+                                            \
+                                            ) AS z \
+                                        WHERE z.Ranking = 1\
+                                        ) AS y\
+                            \
+                        ON x.PLAYER_ID = y.PLAYER_ID;"
                          ,cobi_betika)
         
 finally:
